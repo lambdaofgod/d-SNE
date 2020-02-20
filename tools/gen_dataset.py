081b5752fb517ee5da7a7808c7fcbaf41618f19d
Omar Ali Sheikh-Omar <sheikhomar@users.noreply.github.com>
2020-02-20 09:54:13 +0100

    Refactor.

--- tools/gen_dataset.py
+++ tools/gen_dataset.py
@@ -14,10 +14,10 @@ class DatasetGenerator:
     def __init__(self):
         pass
 
-    def download_file(self, url: str, storage_dir: str, chunk_size=1024):
+    def download_file(self, url: str, storage_dir: Path, chunk_size=1024):
         file_name = url.split('/')[-1]
-        file_path = os.path.join(storage_dir, file_name)
-        with open(file_path, 'wb') as f:
+        file_path = storage_dir / file_name
+        with open(str(file_path), 'wb') as f:
             resp = requests.get(url, stream=True)
             file_size = resp.headers.get('content-length')
             print('Downloading from {}. Total bytes {}'.format(url, file_size))
@@ -34,7 +34,8 @@ class DatasetGenerator:
                 pbar.close()
         return file_path
 
-    def unzip_file(self, file_path: str, clean_up: bool = True):
+    def unzip_file(self, file_path: Path, clean_up: bool = True):
+        file_path = str(file_path)
         with gzip.open(file_path, 'rb') as compressed_file:
             with open(file_path.replace('.gz', ''), 'wb') as uncompressed_file:
                 shutil.copyfileobj(compressed_file, uncompressed_file)
@@ -43,9 +44,9 @@ class DatasetGenerator:
 
 
 class MNIST(DatasetGenerator):
-    def __init__(self, root):
+    def __init__(self, root_dir: Path):
         super().__init__()
-        self.root = root
+        self.root_dir = root_dir
 
     def generate(self):
         self.download()
@@ -59,38 +60,38 @@ class MNIST(DatasetGenerator):
                  test_labels_url: str = 'http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz',
                  ):
         for url in [train_images_url, train_labels_url, test_images_url, test_labels_url]:
-            file_path = self.download_file(url, self.root)
+            file_path = self.download_file(url, self.root_dir)
             self.unzip_file(file_path)
 
     def read(self):
-        def read_img(path):
-            with open(path, 'rb') as fimg:
+        def read_img(file_path: Path):
+            with open(str(file_path), 'rb') as fimg:
                 magic, num, rows, cols = struct.unpack(">IIII", fimg.read(16))
                 img = np.fromfile(fimg, dtype=np.uint8).reshape(-1, rows, cols)
 
             return img
 
-        def read_lbl(path):
-            with open(path, 'rb') as flbl:
+        def read_lbl(file_path: Path):
+            with open(str(file_path), 'rb') as flbl:
                 magic, num = struct.unpack(">II", flbl.read(8))
                 lbl = np.fromfile(flbl, dtype=np.int8)
 
             return lbl
 
-        train_img = read_img(os.path.join(self.root, 'train-images-idx3-ubyte'))
-        train_lbl = read_lbl(os.path.join(self.root, 'train-labels-idx1-ubyte'))
+        train_img = read_img(self.root_dir / 'train-images-idx3-ubyte')
+        train_lbl = read_lbl(self.root_dir / 'train-labels-idx1-ubyte')
 
-        test_img = read_img(os.path.join(self.root, 't10k-images-idx3-ubyte'))
-        test_lbl = read_lbl(os.path.join(self.root, 't10k-labels-idx1-ubyte'))
+        test_img = read_img(self.root_dir / 't10k-images-idx3-ubyte')
+        test_lbl = read_lbl(self.root_dir / 't10k-labels-idx1-ubyte')
 
         self.dataset = {'TR': [train_img, train_lbl], 'TE': [test_img, test_lbl]}
 
     def dump(self):
-        with open(os.path.join(self.root, self.__class__.__name__ + '.pkl'), 'wb') as fout:
+        with open(os.path.join(self.root_dir, self.__class__.__name__ + '.pkl'), 'wb') as fout:
             pk.dump(self.dataset, fout)
 
 
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dir', required=True)
     parser.add_argument('-d', '--dataset', default='mnist', help='dataset')
@@ -101,7 +102,11 @@ if __name__ == '__main__':
     root_dir.mkdir(parents=True, exist_ok=True)
 
     if args.dataset.lower() == 'mnist':
-        dataset = MNIST(str(root_dir))
+        dataset = MNIST(root_dir)
         dataset.generate()
     else:
         print('Required download the dataset and packed by yourself, sorry for inconvenience')
+
+
+if __name__ == '__main__':
+    main()
