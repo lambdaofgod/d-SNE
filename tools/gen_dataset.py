40a41a8fb21ac8bbcf9ac50ac3ea3f1bdd90dc67
Omar Ali Sheikh-Omar <sheikhomar@users.noreply.github.com>
2020-02-20 11:19:02 +0100

    Data generator for USPS.

--- tools/gen_dataset.py
+++ tools/gen_dataset.py
@@ -1,5 +1,7 @@
 import os
 import argparse
+from typing import List
+
 import numpy as np
 import struct
 import pickle as pk
@@ -11,12 +13,13 @@ import requests
 
 
 class DatasetGenerator:
-    def __init__(self):
-        pass
+    def __init__(self, root_dir: Path, name: str):
+        self.root_dir = root_dir
+        self.name = name
 
-    def download_file(self, url: str, storage_dir: Path, chunk_size=1024):
+    def download_file(self, url: str, chunk_size=1024):
         file_name = url.split('/')[-1]
-        file_path = storage_dir / file_name
+        file_path = self.root_dir / file_name
         with open(str(file_path), 'wb') as f:
             resp = requests.get(url, stream=True)
             file_size = resp.headers.get('content-length')
@@ -42,70 +45,96 @@ class DatasetGenerator:
         if clean_up:
             os.remove(file_path)
 
+    def dump(self, train_img: np.ndarray, train_lbl: np.ndarray, test_img: np.ndarray, test_lbl: np.ndarray):
+        dataset = {'TR': [train_img, train_lbl], 'TE': [test_img, test_lbl]}
+        file_path = self.root_dir / (self.name + '.pkl')
+        print('Creating pickle file {}...'.format(file_path))
+        with open(str(file_path), 'wb') as f:
+            pk.dump(dataset, f)
 
-class MNIST(DatasetGenerator):
-    def __init__(self, root_dir: Path):
-        super().__init__()
-        self.root_dir = root_dir
-
-    def generate(self):
-        self.download()
-        self.read()
-        self.dump()
-
-    def download(self,
-                 train_images_url: str = 'http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz',
-                 train_labels_url: str = 'http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz',
-                 test_images_url: str = 'http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz',
-                 test_labels_url: str = 'http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz',
-                 ):
-        for url in [train_images_url, train_labels_url, test_images_url, test_labels_url]:
-            file_path = self.download_file(url, self.root_dir)
+    def download_and_unzip(self, urls: List[str]):
+        for url in urls:
+            file_path = self.download_file(url)
             self.unzip_file(file_path)
 
-    def read(self):
-        def read_img(file_path: Path):
-            with open(str(file_path), 'rb') as fimg:
-                magic, num, rows, cols = struct.unpack(">IIII", fimg.read(16))
-                img = np.fromfile(fimg, dtype=np.uint8).reshape(-1, rows, cols)
 
-            return img
+def generate_mnist(root_dir: Path):
+    ds_gen = DatasetGenerator(root_dir, 'mnist')
 
-        def read_lbl(file_path: Path):
-            with open(str(file_path), 'rb') as flbl:
-                magic, num = struct.unpack(">II", flbl.read(8))
-                lbl = np.fromfile(flbl, dtype=np.int8)
+    urls = [
+        'http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz',
+        'http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz',
+        'http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz',
+        'http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz'
+    ]
+    ds_gen.download_and_unzip(urls)
 
-            return lbl
+    def read_img(file_path: Path):
+        with open(str(file_path), 'rb') as fimg:
+            magic, num, rows, cols = struct.unpack(">IIII", fimg.read(16))
+            img = np.fromfile(fimg, dtype=np.uint8).reshape(-1, rows, cols)
 
-        train_img = read_img(self.root_dir / 'train-images-idx3-ubyte')
-        train_lbl = read_lbl(self.root_dir / 'train-labels-idx1-ubyte')
+        return img
 
-        test_img = read_img(self.root_dir / 't10k-images-idx3-ubyte')
-        test_lbl = read_lbl(self.root_dir / 't10k-labels-idx1-ubyte')
+    def read_lbl(file_path: Path):
+        with open(str(file_path), 'rb') as flbl:
+            magic, num = struct.unpack(">II", flbl.read(8))
+            lbl = np.fromfile(flbl, dtype=np.int8)
 
-        self.dataset = {'TR': [train_img, train_lbl], 'TE': [test_img, test_lbl]}
+        return lbl
 
-    def dump(self):
-        with open(os.path.join(self.root_dir, self.__class__.__name__ + '.pkl'), 'wb') as fout:
-            pk.dump(self.dataset, fout)
+    train_img = read_img(root_dir / 'train-images-idx3-ubyte')
+    train_lbl = read_lbl(root_dir / 'train-labels-idx1-ubyte')
+    test_img = read_img(root_dir / 't10k-images-idx3-ubyte')
+    test_lbl = read_lbl(root_dir / 't10k-labels-idx1-ubyte')
+
+    ds_gen.dump(train_img=train_img, train_lbl=train_lbl, test_img=test_img, test_lbl=test_lbl)
+
+
+def generate_usps(root_dir: Path):
+    ds_gen = DatasetGenerator(root_dir, 'usps')
+
+    urls = [
+        'https://web.stanford.edu/~hastie/ElemStatLearn/datasets/zip.train.gz',
+        'https://web.stanford.edu/~hastie/ElemStatLearn/datasets/zip.test.gz'
+    ]
+    ds_gen.download_and_unzip(urls)
+
+    def read_images_and_labels(file_path: Path):
+        with open(str(file_path), 'r') as f:
+            data_array = np.loadtxt(f)
+            labels = data_array[:, 0].astype(np.int)
+            images = data_array[:, 1:].reshape(-1, 16, 16)
+        return images, labels
+
+    train_img, train_lbl = read_images_and_labels(root_dir / 'zip.train')
+    test_img, test_lbl = read_images_and_labels(root_dir / 'zip.test')
+
+    ds_gen.dump(train_img=train_img, train_lbl=train_lbl, test_img=test_img, test_lbl=test_lbl)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dir', required=True)
-    parser.add_argument('-d', '--dataset', default='mnist', help='dataset')
+    parser.add_argument('-d', '--dataset')
 
     args = parser.parse_args()
+
+    dataset_name = args.dataset.lower()
 
     root_dir = Path(args.dir)
     root_dir.mkdir(parents=True, exist_ok=True)
 
-    if args.dataset.lower() == 'mnist':
-        dataset = MNIST(root_dir)
-        dataset.generate()
+    generators = {
+        'mnist': generate_mnist,
+        'usps': generate_usps
+    }
+
+    if dataset_name in generators:
+        generators[dataset_name](root_dir)
+        print('Done!')
     else:
-        print('Required download the dataset and packed by yourself, sorry for inconvenience')
+        print('Dataset {} is not supported.'.format(dataset_name))
 
 
 if __name__ == '__main__':
