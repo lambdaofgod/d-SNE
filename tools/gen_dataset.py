4970dd090124e94ea70c84ca5d8b7cf72f5251ed
Omar Ali Sheikh-Omar <sheikhomar@users.noreply.github.com>
2020-02-20 09:49:12 +0100

    Script to generate MNIST dataset.

--- tools/gen_dataset.py
+++ tools/gen_dataset.py
@@ -3,11 +3,64 @@ import argparse
 import numpy as np
 import struct
 import pickle as pk
+import gzip
+import shutil
+from pathlib import Path
+from tqdm import tqdm
+import requests
 
 
-class MNIST:
+class DatasetGenerator:
+    def __init__(self):
+        pass
+
+    def download_file(self, url: str, storage_dir: str, chunk_size=1024):
+        file_name = url.split('/')[-1]
+        file_path = os.path.join(storage_dir, file_name)
+        with open(file_path, 'wb') as f:
+            resp = requests.get(url, stream=True)
+            file_size = resp.headers.get('content-length')
+            print('Downloading from {}. Total bytes {}'.format(url, file_size))
+
+            if file_size is None:
+                f.write(resp.content)
+            else:
+                file_size = int(file_size)
+                pbar = tqdm(total=file_size, unit='B', unit_scale=True, desc=file_name)
+                for chunk in resp.iter_content(chunk_size=chunk_size):
+                    if chunk:
+                        f.write(chunk)
+                        pbar.update(chunk_size)
+                pbar.close()
+        return file_path
+
+    def unzip_file(self, file_path: str, clean_up: bool = True):
+        with gzip.open(file_path, 'rb') as compressed_file:
+            with open(file_path.replace('.gz', ''), 'wb') as uncompressed_file:
+                shutil.copyfileobj(compressed_file, uncompressed_file)
+        if clean_up:
+            os.remove(file_path)
+
+
+class MNIST(DatasetGenerator):
     def __init__(self, root):
+        super().__init__()
         self.root = root
+
+    def generate(self):
+        self.download()
+        self.read()
+        self.dump()
+
+    def download(self,
+                 train_images_url: str = 'http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz',
+                 train_labels_url: str = 'http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz',
+                 test_images_url: str = 'http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz',
+                 test_labels_url: str = 'http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz',
+                 ):
+        for url in [train_images_url, train_labels_url, test_images_url, test_labels_url]:
+            file_path = self.download_file(url, self.root)
+            self.unzip_file(file_path)
 
     def read(self):
         def read_img(path):
@@ -39,14 +92,16 @@ class MNIST:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dir', )
+    parser.add_argument('--dir', required=True)
     parser.add_argument('-d', '--dataset', default='mnist', help='dataset')
 
     args = parser.parse_args()
 
+    root_dir = Path(args.dir)
+    root_dir.mkdir(parents=True, exist_ok=True)
+
     if args.dataset.lower() == 'mnist':
-        dataset = MNIST(args.dir)
-        dataset.read()
-        dataset.dump()
+        dataset = MNIST(str(root_dir))
+        dataset.generate()
     else:
         print('Required download the dataset and packed by yourself, sorry for inconvenience')
