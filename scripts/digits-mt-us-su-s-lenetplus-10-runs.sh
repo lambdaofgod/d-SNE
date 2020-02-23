ea1a5b7e50b22220ee8b09c1aad3ea69dc26c038
Omar Ali Sheikh-Omar <sheikhomar@users.noreply.github.com>
2020-02-23 18:38:22 +0100

    Script to run experiments 10 times.

---
+++ scripts/digits-mt-us-su-s-lenetplus-10-runs.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Experiment on MNIST -> USPS (10 runs)
+
+for RUN in 1 2 3 4 5 6 7 8 9 10
+do
+  for SAMPLES in 1 3 5 7
+  do
+    python main_sda.py \
+        --cfg cfg/digits-s-1.json \
+        --src MT \
+        --tgt US \
+        --method dsne \
+        --nc 10 \
+        --size 32 \
+        --bb lenetplus \
+        --dropout \
+        --train-src \
+        --lr 0.1 \
+        --gpus 0 \
+        --end-epoch 100 \
+        --lr-epoch 50 \
+        --log-itv 0 \
+        --postfix s$SAMPLES-0.5-l2n-run$RUN \
+        --embed-size 80 \
+        --l2n \
+        --alpha 0.5
+  done
+done
